8534aa0119fd6590367326028801fac92a0d4572
turov <ilya.turov@gmail.com>
2018-09-06 20:37:46 +0300

    💽

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,28 +1,30 @@
+'use strict';
+
+(function () {
+
 var nav = document.querySelector('.nav');
 var btn = document.querySelector('.nav__btn');
 var page = document.querySelector('.page');
 var blackWrapper = document.querySelector('.wrapper-black');
+var links = document.querySelectorAll('.nav__link');
 
 nav.classList.remove('nav--opened');
 nav.classList.add('nav--closed');
-
-// btn.addEventListener('click', function () {
-//   if (nav.classList.contains('nav--closed')) {
-//     nav.classList.remove('nav--closed');
-//     nav.classList.add('nav--opened');
-//     page.classList.add('page--fixed');
-//     blackWrapper.classList.add('wrapper-black--active')
-//   } else {
-//     nav.classList.add('nav--closed');
-//     nav.classList.remove('nav--opened');
-//     nav.classList.remove('page--fixed')
-//     blackWrapper.classList.remove('wrapper-black--active')
-//   }
-// });
-
 
 btn.onclick = function () {
   nav.classList.toggle('nav--opened');
   page.classList.toggle('page--fixed');
   blackWrapper.classList.toggle('wrapper-black--active')
 };
+
+  links.forEach(function (element) {
+    element.onclick = function () {
+      links.forEach(function (el) {
+        nav.classList.remove('nav--opened');
+        page.classList.remove('page--fixed');
+        blackWrapper.classList.remove('wrapper-black--active')
+      });
+    }
+  });
+
+})();
